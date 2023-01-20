cd05198f04bab12ac675e37ee214650b1f99568b
unknown <matias2k00@gmail.com>
2023-01-19 23:07:27 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import Cards from './components/cards/Cards';
 import NavBar from './components/navBar/NavBar';
-import { Route } from 'react-router-dom';
-import Card from './../../rick_and_morty/src/components/card/Card';
-import style from './index.css';
+import { Route, Routes } from 'react-router-dom';
 import Count from './components/contador/Count';
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <div className={style}>
-        <Route exact path='/cards'>
-          <Cards />{' '}
-        </Route>
-        <Route exact path='/cards/:id'>
-          <Card />{' '}
-        </Route>
-        <Route exact path='/count'>
-          <Count />
-        </Route>
-      </div>
+      <Routes>
+        <Route exact path='/' element={<NavBar />} />
+        <Route exact path='/cards' element={<Cards />} />
+        <Route exact path='/count' element={<Count />} />
+      </Routes>
     </div>
   );
 }
