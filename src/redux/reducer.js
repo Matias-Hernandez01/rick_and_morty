8a9c9ca229e39c19875035840f2c1ffb3c5a5c28
unknown <matias2k00@gmail.com>
2023-01-20 04:10:45 -0300

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,34 +1,25 @@
-import { INCREMENT, DECREMENT, RESET, GET_API } from './actions';
+import { GET_API, GET_INPUT_VALUE } from './actions';
 
 const initialState = {
-  stateCount: 0,
   personajes: [],
+  input: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        stateCount: state.stateCount + 1,
-      };
-
-    case DECREMENT:
-      return {
-        ...state,
-        stateCount: state.stateCount - 1,
-      };
-
     case GET_API:
       return {
         ...state,
         personajes: action.payload,
       };
-
-    case RESET:
+    case GET_INPUT_VALUE:
       return {
         ...state,
-        stateCount: 0,
+        personajes: [
+          ...state.personajes.filter(
+            (element) => element.name === action.payload
+          ),
+        ],
       };
 
     default:
