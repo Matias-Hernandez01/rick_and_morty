0b3645e0c72b1a8652d795370e83531ed97bd766
unknown <matias2k00@gmail.com>
2023-01-27 23:58:02 -0300

    card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import style from './card.module.css';
+import * as actions from '../../redux/actions';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Card({ name, species, status, image, id }) {
+  const dispatch = useDispatch();
   return (
     <div className={style.container}>
       <div className={style.card}>
-        <h4>{id}</h4>
-        <img src={image} alt={name} />
+        <div className={style.containerButtonDelete}>
+          <button
+            className={style.buttonDelete}
+            onClick={() => {
+              dispatch(actions.cardsDelete(id));
+            }}
+          >
+            Delete
+          </button>
+        </div>
+        <Link to={`/cards/${id}`}>
+          <img src={image} alt={name} />
+        </Link>
         <div>
           <h1>{name}</h1>
           <h2>{species}</h2>
-          <h3>{status}</h3>
+          <h3
+            className={
+              status === 'alive'
+                ? style.green
+                  ? status === 'dead'
+                  : style.red
+                : style.orange
+            }
+          >
+            {status}
+          </h3>
         </div>
       </div>
     </div>
