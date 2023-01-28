5d64663b2fefaa6d6e4f2274c36edb98c1cbfeb8
unknown <matias2k00@gmail.com>
2023-01-28 00:03:18 -0300

    navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,31 +1,62 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './NavBar.module.css';
-import LogoGif from './../../imagenes/LogoGif2.gif';
+import linkedin from '../../imagenes/linkedin (1).png';
+import github from '../../imagenes/github.png';
 
 const NavBar = () => {
   return (
     <div className={style.conteiner}>
-      <ul className={style.menu}>
-        <li>
-          <NavLink to='/'>
-            <img src={LogoGif} alt='Logo rick and morty' />
-          </NavLink>
-        </li>
-        <div className={style.conteinerTitle}>
-          <h1>The Rick and Morty</h1>
-        </div>
-        <div className={style.menuCards}>
-          <li className={style.CardsP}>
+      <div className={style.header}>
+        <ul className={style.menu}>
+          <h1 className={style.title}>Rick and Morty</h1>
+          <li>
             <NavLink to='/cards'>
-              <p>Cards</p>
+              <p className={style.cardsP}>Cards</p>
             </NavLink>
           </li>
           <li>
-            <p>Api</p>
+            <a
+              className={style.api}
+              href='https://rickandmortyapi.com/api/character'
+              target='_blank'
+            >
+              Api
+            </a>
           </li>
-        </div>
-      </ul>
+          <li>
+            <img className={style.linkedin} src={linkedin} />
+          </li>
+          <li>
+            <img className={style.github} src={github} />
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <div className={style.containerSection}>
+        <section className={style.section}>
+          <img
+            src='https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+            alt='Dwayne'
+          ></img>
+          <img
+            src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+            alt='Kyle'
+          ></img>
+          <img
+            src='https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+            alt='Steve'
+          ></img>
+          <img
+            src='https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+            alt='VampireMaster'
+          ></img>
+          <img
+            src='https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+            alt='Steve2'
+          ></img>
+        </section>
+      </div>
     </div>
   );
 };
