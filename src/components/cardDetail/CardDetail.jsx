98fb5acd2f0a89366e8b9f704836a1a20da25818
unknown <matias2k00@gmail.com>
2023-02-02 04:39:45 -0300

    componente detail terminado

--- src/components/cardDetail/CardDetail.jsx
+++ src/components/cardDetail/CardDetail.jsx
@@ -5,6 +5,16 @@ import { useEffect } from 'react';
 import style from './cardDetail.module.css';
 import imgCard from '../../imagenes/Cards.png';
 import home from '../../imagenes/Home.png';
+import alive from '../../imagenes/Alive.png';
+import dead from '../../imagenes/Dead.png';
+import unknown from '../../imagenes/Unknown.png';
+import human from '../../imagenes/persona.png';
+import aliens from '../../imagenes/alien.png';
+import male from '../../imagenes/male.png';
+import female from '../../imagenes/female.png';
+import linkedin from '../../imagenes/Linkedin.png';
+import instagram from '../../imagenes/instagram (1).png';
+import github from '../../imagenes/github (1).png';
 
 const CardDetail = () => {
   const navigate = useNavigate();
@@ -45,11 +55,64 @@ const CardDetail = () => {
         </div>
         <div className={style.containerDetail}>
           <h1>{personaje.name}</h1>
-          <h2>{personaje.status}</h2>
-          <h2>{personaje.species}</h2>
-          <h3>{personaje.gender}</h3>
-          <p>{personaje.location?.name}</p>
+          {personaje.status === 'Alive' ? (
+            <div className={style.divAlive}>
+              <img className={style.alive} src={alive} alt='alive' />
+              <h2 className={style.statusAlive}>{personaje.status}</h2>
+            </div>
+          ) : personaje.status === 'Dead' ? (
+            <div className={style.divDead}>
+              <img className={style.statusDead} src={dead} alt='dead' />
+              <h2 className={style.dead}>{personaje.status}</h2>
+            </div>
+          ) : (
+            <div className={style.divUnknown}>
+              <img
+                className={style.statusUnknown}
+                src={unknown}
+                alt='unknown'
+              />
+              <h2 className={style.unknown}>{personaje.status}</h2>
+            </div>
+          )}
+          {personaje.species === 'Human' ? (
+            <div className={style.divHuman}>
+              <img className={style.imgHuman} src={human} alt='human' />
+              <h2 className={style.human}>{personaje.species}</h2>
+            </div>
+          ) : (
+            <div className={style.divAlien}>
+              <img className={style.imgAlien} src={aliens} alt='alien' />
+              <h2 className={style.alien}>{personaje.species}</h2>
+            </div>
+          )}
+          {personaje.gender === 'Male' ? (
+            <div className={style.divMale}>
+              <img className={style.imgMale} src={male} alt='male' />
+              <h2 className={style.male}>{personaje.gender}</h2>
+            </div>
+          ) : (
+            <div className={style.divFemale}>
+              <img className={style.imgFemale} src={female} alt='female' />
+              <h2 className={style.female}>{personaje.gender}</h2>
+            </div>
+          )}
+          <p className={style.location}>{personaje.location?.name}</p>
         </div>
+      </div>
+      <div className={style.containerContact}>
+        <a
+          href='https://www.linkedin.com/in/matiashernandez00/'
+          target='_blank'
+        >
+          <img className={style.linkedin} src={linkedin} alt='linkedin' />
+        </a>
+        <a href='https://github.com/Matias-Hernandez01' target='_blank'>
+          <img className={style.github} src={github} alt='github' />
+        </a>
+        <a href='https://www.instagram.com/matias.dev/' target='_blank'>
+          <img className={style.instagram} src={instagram} alt='instagram' />
+        </a>
       </div>
     </div>
   );
