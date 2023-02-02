90f6bb9062d1dece7d74e6e2951d5c053f50d2b3
unknown <matias2k00@gmail.com>
2023-02-01 23:47:11 -0300

    componente cardDetail finalizado

--- src/components/cardDetail/CardDetail.jsx
+++ src/components/cardDetail/CardDetail.jsx
@@ -3,6 +3,8 @@ import { cardDetail } from '../../redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import style from './cardDetail.module.css';
+import imgCard from '../../imagenes/Cards.png';
+import home from '../../imagenes/Home.png';
 
 const CardDetail = () => {
   const navigate = useNavigate();
@@ -16,24 +18,38 @@ const CardDetail = () => {
 
   return (
     <div className={style.container}>
-      <div>
+      <div className={style.containerButton}>
         <button
           className={style.backToCards}
           onClick={() => navigate('/cards')}
         >
-          Back to Cards
+          <img className={style.imgCard} src={imgCard} alt='cards' />
+          Cards
+        </button>
+        <button
+          className={style.buttonHome}
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          <img src={home} alt='home' className={style.imgHome} /> Home
         </button>
       </div>
       <div className={style.containerCardsDetail}>
-        <img
-          className={style.image}
-          src={personaje.image}
-          alt={personaje.name}
-        />
-        <h1>{personaje.name}</h1>
-        <h2>{personaje.status}</h2>
-        <h2>{personaje.species}</h2>
-        <p>{personaje.gender}</p>
+        <div className={style.containerImgDetail}>
+          <img
+            className={style.imagenDeCard}
+            src={personaje.image}
+            alt={personaje.name}
+          />
+        </div>
+        <div className={style.containerDetail}>
+          <h1>{personaje.name}</h1>
+          <h2>{personaje.status}</h2>
+          <h2>{personaje.species}</h2>
+          <h3>{personaje.gender}</h3>
+          <p>{personaje.location?.name}</p>
+        </div>
       </div>
     </div>
   );
