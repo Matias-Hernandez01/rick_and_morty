6c9cd6559d8f8b502cdf3aae057676034a66c6db
unknown <matias2k00@gmail.com>
2023-02-17 23:22:22 -0300

    aoo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cards from './components/cards/Cards';
 import NavBar from './components/navBar/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import CardDetail from './components/cardDetail/CardDetail';
+import Aboutme from './components/aboutMe/Aboutme';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' element={<NavBar />} />
         <Route exact path='/cards' element={<Cards />} />
         <Route path='/cards/:id' element={<CardDetail />} />
+        <Route path='/aboutme' element={<Aboutme />} />
       </Routes>
     </div>
   );
