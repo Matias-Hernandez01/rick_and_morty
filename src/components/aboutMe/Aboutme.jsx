3c6fb6373baa80293c25c0f3ff533ecc2b99b461
unknown <matias2k00@gmail.com>
2023-03-07 23:44:48 -0300

    ab

--- src/components/aboutMe/Aboutme.jsx
+++ src/components/aboutMe/Aboutme.jsx
@@ -35,8 +35,7 @@ const Aboutme = () => {
                   <b className={style.linkHenry}>Henry</b>
                 </a>
                 . Tomé este proyecto como un desafio para ir aplicando los
-                conocimientos que fuí adquiriendo durante la cursada, aplicando
-                React, Redux y CSS
+                conocimientos que fuí adquiriendo durante la cursada
               </h2>
             </div>
           </div>
