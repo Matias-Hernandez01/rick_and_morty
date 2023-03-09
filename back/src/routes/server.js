f4aa43b2c6416ff1f41d35861b89a1f6b6c726b0
unknown <matias2k00@gmail.com>
2023-03-09 14:54:58 -0300

    agregando backend

--- back/src/routes/server.js
+++ back/src/routes/server.js
@@ -5,8 +5,11 @@ const PORT = 3001;
 
 http
   .createServer((req, res) => {
-    if (req.url.includes('rickandmorty/characters')) {
+    if (req.url.includes('rickandmorty/character')) {
       let id = req.url.split('/').at(-1);
+      let characterFilter = characters.find((char) => char.id === Number(id));
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(characterFilter));
     }
   })
-  .listen(3001, 'localhost');
+  .listen(PORT, 'localhost');
