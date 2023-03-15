bd41e799a7f3749194cc5efea2362b0b9eac155a
unknown <matias2k00@gmail.com>
2023-03-14 23:24:02 -0300

    redux

--- front/src/redux/actions.js
+++ front/src/redux/actions.js
@@ -35,7 +35,7 @@ export const getApi = () => {
 };
 export const cardDetail = (id) => {
   return (dispatch) => {
-    fetch(`https://rickandmortyapi.com/api/character/:${id}`)
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
       .then((response) => response.json())
       .then((data) => dispatch({ type: CARD_DETAIL, payload: data }));
   };
