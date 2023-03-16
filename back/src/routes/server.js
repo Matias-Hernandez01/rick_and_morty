852b0114f32c1ff73b519581577cf5f822ed6239
unknown <matias2k00@gmail.com>
2023-03-16 20:37:02 -0300

    agregando ruta en mi servidor

--- back/src/routes/server.js
+++ back/src/routes/server.js
@@ -1,4 +1,13 @@
 const http = require('http');
 const PORT = 3001;
+const getCharById = require('../controllers/getCharById');
 
-http.createServer((req, res) => {}).listen(PORT, 'localhost');
+http
+  .createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.url.includes('detail')) {
+      let id = req.url.split('/').at(-1);
+      getCharById(res, id);
+    }
+  })
+  .listen(PORT, 'localhost');
