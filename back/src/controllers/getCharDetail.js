f3554ce914052443ecc85136302a3d9a9aa6c267
unknown <matias2k00@gmail.com>
2023-03-16 20:36:35 -0300

    peticion de character por id desde el backend

---
+++ back/src/controllers/getCharDetail.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const getCharDetail = (res, id) => {
+  axios
+    .get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((res) => res.data)
+    .then((data) => {
+      let characterDetail = {
+        image: data.image,
+        name: data.name,
+        gender: data.gender,
+        status: data.status,
+        origin: data.origin,
+        species: data.species,
+      };
+      res
+        .writeHead(200, { 'Content-Type': 'application/json' })
+        .end(JSON.stringify(characterDetail));
+    })
+    .catch((err) =>
+      res
+        .writeHead(500, { 'Content-Type': 'text/plain' })
+        .end(`The character with the id: ${id}, is not available`)
+    );
+};
+
+module.exports = getCharDetail;
