5485352bbaabbe2b67de87a282e5d893edf10236
unknown <matias2k00@gmail.com>
2023-03-16 16:05:55 -0300

    controller getCharById

---
+++ back/src/controllers/getCharById.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+const getCharById = (res, id) => {
+  axios
+    .get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((res) => res.data)
+    .then((data) => {
+      let characters = {
+        image: data.image,
+        name: data.name,
+        gender: data.gender,
+        species: data.species,
+      };
+      res
+        .writeHead(200, { 'Content-Type': 'application/json' })
+        .end(JSON.stringify(characters));
+    })
+    .catch((err) =>
+      res
+        .writeHead(500, { 'Content-Type': 'text/plain' })
+        .end(`The character with the id ${id} is not available`)
+    );
+};
+
+module.exports = getCharById;
