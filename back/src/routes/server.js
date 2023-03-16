9b615b5ec78414ac6ecb61f27b82fbc0137dd35c
unknown <matias2k00@gmail.com>
2023-03-16 15:14:41 -0300

    modificando server

--- back/src/routes/server.js
+++ back/src/routes/server.js
@@ -1,15 +1,4 @@
 const http = require('http');
-const characters = require('../utils/data');
-
 const PORT = 3001;
 
-http
-  .createServer((req, res) => {
-    if (req.url.includes('rickandmorty/character')) {
-      let id = req.url.split('/').at(-1);
-      let characterFilter = characters.find((char) => char.id === Number(id));
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(characterFilter));
-    }
-  })
-  .listen(PORT, 'localhost');
+http.createServer((req, res) => {}).listen(PORT, 'localhost');
